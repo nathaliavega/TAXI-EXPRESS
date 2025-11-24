17d5eaf314b3bcfc452bdb414831c4215d6f9aea
nathaliavega <164430365+nathaliavega@users.noreply.github.com>
2025-11-24 11:10:44 -0500

    Cambio T.E.P.N.v.g

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,6 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies = '*';
+    
 
     /**
      * The headers that should be used to detect proxies.
@@ -20,6 +21,7 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers =
+        Request::HEADER_X_FORWARDED_ALL|
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
